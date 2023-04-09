0cdca95f0352ae023c68e54884cf906e6b180ff7
Aaron Morse <morse.aaron1985@gmail.com>
2023-04-09 14:20:44 -0700

    Fixed the checkout button hover and the accout image hover

--- src/components/account/Account.jsx
+++ src/components/account/Account.jsx
@@ -2,5 +2,9 @@ import styles from "./account.module.css";
 import imageAvatar from "../../assets/images/image-avatar.png";
 
 export default function Account() {
-  return <img src={imageAvatar} className={styles.accountImage} />;
+  return (
+    <div className={styles.wrapper}>
+      <img src={imageAvatar} className={styles.accountImage} />
+    </div>
+  );
 }
